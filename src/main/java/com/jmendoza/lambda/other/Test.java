c3f619bf10bd06ec8a42069622aba5d54e0f4b3f
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-03-20 20:31:33 -0400

    Update Lambdas

--- src/main/java/com/jmendoza/lambda/other/Test.java
+++ src/main/java/com/jmendoza/lambda/other/Test.java
@@ -1,5 +1,9 @@
 package com.jmendoza.lambda.other;
 
+import com.jmendoza.stream.model.User;
+
+import java.util.*;
+
 public class Test {
 
     private int operate(int a, int b, Calculable calculable) {
@@ -13,5 +17,17 @@ public class Test {
         System.out.println("Multiply: " + test.operate(5, 7, (x, y) -> x * y));
 
         System.out.println("Sum: " + test.operate(5, 7, (x, y) -> x + y));
+
+        // Sort
+        List<User> userList = Arrays.asList(
+                new User("jmendoza", 1),
+                new User("amendoza", 2),
+                new User("noche", 3),
+                new User("agil", 4)
+        );
+        System.out.println(userList.toString());
+        //Collections.sort(userList,(p1,p2)->p1.getUserName().compareTo(p2.getUserName()));
+        Collections.sort(userList, Comparator.comparing(User::getUserName));
+        System.out.println(userList.toString());
     }
 }
