1f884e27cc26147b7378beb14ed9b2cfff6c69ad
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-03-31 16:41:37 -0400

    Update Concurrency

--- src/main/java/com/jmendoza/concurrency/executors/ExecutorServiceTest.java
+++ src/main/java/com/jmendoza/concurrency/executors/ExecutorServiceTest.java
@@ -3,6 +3,11 @@ package com.jmendoza.concurrency.executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Task Type            Ideal pool size (ThreadPool)
+ * CPU Intensive        CPU Core count
+ * IO Intensive         Hig
+ */
 public class ExecutorServiceTest {
     public static void main(String[] args) {
 
@@ -10,7 +15,7 @@ public class ExecutorServiceTest {
         ExecutorService executorService = Executors.newFixedThreadPool(coreCount);
         System.out.println("Available Processors: " + coreCount);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             executorService.execute(new CpuTask("CpuTask-" + i));
         }
         executorService.shutdown();
