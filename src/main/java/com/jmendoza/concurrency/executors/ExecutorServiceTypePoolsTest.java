0422dad063d12213873fe1ae9767c4c3b714595e
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-01 18:50:43 -0400

    Update Concurrency - Executor Service Type Pools

--- src/main/java/com/jmendoza/concurrency/executors/ExecutorServiceTypePoolsTest.java
+++ src/main/java/com/jmendoza/concurrency/executors/ExecutorServiceTypePoolsTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class ExecutorServiceTypePools {
+public class ExecutorServiceTypePoolsTest {
 
     public static void main(String[] args) throws InterruptedException {
 
