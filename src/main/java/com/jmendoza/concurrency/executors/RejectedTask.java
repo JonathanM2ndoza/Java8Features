d7e35d95e852fb790d49b2ae1e6828f0bbbf4ea8
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-01 17:19:49 -0400

    Update Concurrency - RejectedExecutionHandler

---
+++ src/main/java/com/jmendoza/concurrency/executors/RejectedTask.java
@@ -0,0 +1,34 @@
+package com.jmendoza.concurrency.executors;
+
+public class RejectedTask implements Runnable {
+
+    private int taskId;
+    private String taskName;
+
+    public RejectedTask(int taskId, String taskName) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        System.out.println(taskName + taskId + " Created");
+    }
+
+    @Override
+    public void run() {
+        Thread curThread = Thread.currentThread();
+        try {
+            taskName = curThread.getName();
+            System.out.println(taskName + " Executing " + taskId);
+            Thread.sleep(1000);
+            System.out.println(taskName + " Completed " + taskId);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RejectedTask{" +
+                "taskId=" + taskId +
+                ", taskName='" + taskName + '\'' +
+                '}';
+    }
+}
