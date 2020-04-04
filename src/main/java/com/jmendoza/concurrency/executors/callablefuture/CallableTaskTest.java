d00f713cfff3d47ac51abc1742ceaeef1cf63c3e
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-04 09:52:17 -0400

    Update Concurrency - Callable and Future

--- src/main/java/com/jmendoza/concurrency/executors/callablefuture/CallableTaskTest.java
+++ src/main/java/com/jmendoza/concurrency/executors/callablefuture/CallableTaskTest.java
@@ -6,13 +6,17 @@ import java.util.concurrent.*;
 
 public class CallableTaskTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         List<Future> futureList = new ArrayList<>();
         for (int i = 0; i < 50; i++) {
             Future<Integer> integerFuture = executorService.submit(new CallableTask());
+            Integer result = integerFuture.get(); // Blocking
+            System.out.println(result);
             futureList.add(integerFuture);
         }
+
+
 
         // Perform some unrelated operations
 
