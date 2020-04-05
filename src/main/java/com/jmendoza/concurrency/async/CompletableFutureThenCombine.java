da6dfcc623bbbcd4ec4b192f71da220fa5b50c40
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-05 12:05:46 -0400

    Update Concurrency - CompletableFuture

--- src/main/java/com/jmendoza/concurrency/async/CompletableFutureThenCombine.java
+++ src/main/java/com/jmendoza/concurrency/async/CompletableFutureThenCombine.java
@@ -7,7 +7,7 @@ public class CompletableFutureThenCombine {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         CompletableFuture<Double> pi = CompletableFuture.supplyAsync(() -> Math.PI);
-        CompletableFuture<Double> radius = CompletableFuture.supplyAsync(() -> new Double(34));
+        CompletableFuture<Double> radius = CompletableFuture.supplyAsync(() -> Double.valueOf(34));
 
         CompletableFuture<Double> result = pi.thenCombine(radius, CompletableFutureThenCombine::calculateAreaCircle);
 
